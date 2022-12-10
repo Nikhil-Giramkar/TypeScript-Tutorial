93f7187236e39ee50bd0cdb0512be7c166eb8e15
niksgiramkar@gmail.com <niksgiramkar@gmail.com>
2022-12-10 11:35:25 +0530

    Hello World

---
+++ hello-world/index.js
@@ -0,0 +1,5 @@
+//Javascript is a subset of Typescript
+//Any JS code is a valid TS code
+console.log('Hello World');
+//Compile TS code as 
+//> tsc index.ts
---
+++ hello-world/index.ts
@@ -0,0 +1,10 @@
+//Javascript is a subset of Typescript
+
+//Any JS code is a valid TS code
+
+//Dowload TS in your poject by npm install typescript --save-dev
+
+console.log('Hello World')
+
+//Compile TS code as 
+//>> npx tsc index.ts
